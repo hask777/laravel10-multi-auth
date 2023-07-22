1fca8ed89cf304b88b7bb7fd109466cbefe099e8
hask777 <haskellisp@gmail.com>
2023-07-22 20:25:57 +0300

    end admin login page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class AdminController extends Controller
 {
     //
     public function AdminDashboard()
     {
-        return view('admin/admin_dashboard', []);
+        return view('admin.index', []);
     }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+    public function AdminLogin(): View
+    {
+        return view('admin.admin_login');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,15 @@ require __DIR__.'/auth.php';
 // Admin Routes
 Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
 });
 
 // Agent Routes
 Route::middleware(['auth', 'role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
 
